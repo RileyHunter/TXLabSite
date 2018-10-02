b4e444c3a4ce2a38bf4f85152858c514708173c3
Riley Hunter <postagestamp@windowslive.com>
2018-10-03 10:57:32 +1300

    Bug fixing

--- app.js
+++ app.js
@@ -9,17 +9,20 @@ function handleOrientation(event) {
 	var beta     = event.beta;
 	var gamma    = event.gamma;
 	
-	if(isFirstIter) {
-		var alphaOffset = alpha;
-		var betaOffset = beta;
-		var gammaOffset = gamma;
-		isFirstIter = false;
-	}
-	
 	console.log("a: " + alpha)
 	console.log("b: " + beta)
 	console.log("g: " + gamma)
+	
+
+	
+
 	if(alpha && beta && gamma) {
+		if(isFirstIter) {
+			var alphaOffset = alpha;
+			var betaOffset = beta;
+			var gammaOffset = gamma;
+			isFirstIter = false;
+		}
 		background.style.transform = "rotate(" + (alpha - alphaOffset) + "deg)";
 		var newTop = ((window.innerHeight / 2) - 155) - ((beta / 90) * (window.innerHeight / 2));
 		background.style.top = "newTop";
