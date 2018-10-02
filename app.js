bc2706a1f229795de8698c4ead0fc7c815b9ec25
Riley Hunter <postagestamp@windowslive.com>
2018-10-03 11:03:31 +1300

    Bug fixing

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 window.addEventListener("deviceorientation", handleOrientation, true);
 
 const background = document.querySelector("#background");
-var isFirstIter = true
+var isFirstIter = true;
 
 function handleOrientation(event) {
 	var absolute = event.absolute;
 	var alpha    = event.alpha;
 	var beta     = event.beta;
 	var gamma    = event.gamma;
-	
-	console.log("a: " + alpha)
-	console.log("b: " + beta)
-	console.log("g: " + gamma)
 
 	if(alpha && beta && gamma) {
+		console.log("a: " + alpha)
+		console.log("b: " + beta)
+		console.log("g: " + gamma)
 		if(isFirstIter) {
 			var alphaOffset = alpha;
 			var betaOffset = beta;
@@ -23,6 +22,11 @@ function handleOrientation(event) {
 		background.style.transform = "rotate(" + (alpha - alphaOffset) + "deg)";
 		var newTop = ((window.innerHeight / 2) - 155) - ((beta / 90) * (window.innerHeight / 2));
 		background.style.top = "newTop";
+	} else {
+		if(isFirstIter) {
+			isFirstIter = false;
+			console.log("No orientation data");
+		}
 	}
 }
 
